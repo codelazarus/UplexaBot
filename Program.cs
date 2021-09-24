9e9d7bbb1150597f70f86013f3c14b5307201ce2
Dragan Stamenkovic <draganstamenkovic92@hotmail.com>
2021-09-24 07:37:30 +0200

    Added more commands

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace UplexaBot
 {
     class Program
     {
-        static TelegramBotClient bot = new TelegramBotClient("2036722940:AAGUuMMbKqAlv5cuyrGWfpkiZMCJwgzsXwA");
+        static TelegramBotClient bot = new TelegramBotClient("API_TOKEN");
 
-        private static string API_KEY = "43348147-32a2-4328-b64b-bd6e6267cdfc";
+        private static string API_KEY = "API_KEY";
 
         static void Main(string[] args)
         {
@@ -21,39 +21,83 @@ namespace UplexaBot
 
         private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
+
             switch (e.Message.Text)
             {
                 case "/price":
-                    bot.SendTextMessageAsync(e.Message.Chat.Id, "$" + GetPrice());
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.GetPrice());
                     break;
+
+                case "/donate":
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.Donate());
+                    break;
+
                 case "/exchanges":
-                    string exchanges = "TradeOgre - LTC/UPX - https://tradeogre.com/exchange/LTC-UPX \n" +
-                                       "Graviex - BTC/UPX - https://graviex.net/markets/upxbtc \n" +
-                                       "STEX - ETH/UPX - https://app.stex.com/en/trading/pair/ETH/UPX/1D \n" +
-                                       "PancakeSwap\n" +
-                                       "Uniswap - https://app.uniswap.org/#/swap?outputCurrency=0xc3d91ffdf44eafc32a9e4489a4efe188489fc183&inputCurrency=ETH&use=v2";
-                    
-                    bot.SendTextMessageAsync(e.Message.Chat.Id, exchanges);
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.GetExchanges());
                     break;
+                
+                case "/max_supply":
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.MaxSupply());
+                    break;
+
+                case "/volume":
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.Volume());
+                    break;
+
+                case "/whitepaper":
+                    bot.SendTextMessageAsync(e.Message.Chat.Id, Uplexa.WhitePaper());
+                    break;
+
                 case "/android_mining_tutorial":
                     bot.SendTextMessageAsync(e.Message.Chat.Id, "https://youtu.be/2qMDTb562L8");
                     break;
+                
                 case "/pc_mining_tutorial":
                     bot.SendTextMessageAsync(e.Message.Chat.Id, "https://www.youtube.com/watch?v=1EPLoMkUAhQ");
                     break;
+                
                 default:
                     bot.SendTextMessageAsync(e.Message.Chat.Id, "Invalid command");
                     break;
             }
         }
 
-
-        private static string GetPrice()
+        class Uplexa
         {
-            ICoinmarketcapClient client = new CoinmarketcapClient(API_KEY);
-            Currency currency = client.GetCurrencyBySlug("uplexa");
-            return currency.Price.ToString();
-        }
+            static ICoinmarketcapClient client = new CoinmarketcapClient(API_KEY);
+            static Currency currency = client.GetCurrencyBySlug("uplexa");
 
+            public static string GetPrice()
+            {
+                return "$" + currency.Price.ToString("N6");
+            }
+
+            public static string GetExchanges()
+            {
+                return "TradeOgre - LTC/UPX - https://tradeogre.com/exchange/LTC-UPX \n" +
+                       "Graviex - BTC/UPX - https://graviex.net/markets/upxbtc \n" +
+                       "STEX - ETH/UPX - https://app.stex.com/en/trading/pair/ETH/UPX/1D \n" +
+                       "PancakeSwap\n" +
+                       "Uniswap - https://app.uniswap.org/#/swap?outputCurrency=0xc3d91ffdf44eafc32a9e4489a4efe188489fc183&inputCurrency=ETH&use=v2";
+            }
+            public static string Donate()
+            {
+                return "https://uplexa.com/donations";
+            }
+            public static string MaxSupply()
+            {
+                return "Max supply is: 10,500,000,000";
+            }
+
+            public static string Volume()
+            {
+                return "24h Volume: $" + currency.Volume24hUsd.ToString("N2");
+            }
+
+            public static string WhitePaper()
+            {
+                return "Link to Whitepaper:\nhttps://uplexa.com/media/uPlexa-Whitepaper-EN.pdf";
+            }
+        }
     }
 }
